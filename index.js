e971f5d883405a484add1f3a854f281ad9efd0cb
Shyaboi <ian.shane.sears@gmail.com>
2021-01-18 15:15:44 -0700

    getting route infor, and making new routes, but all serving the most recent object in for loop.

--- index.js
+++ index.js
@@ -15,34 +15,71 @@ const port = process.env.PORT || 4444;
 // db is importing the var from the connect folder/module
 const db = require("./database/connection/connect");
 // importing the mongoose model created for mongo.
-const uModel = require("./database/models/users");
+const rModel = require("./database/models/routes");
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 // ---------------------------------------------------------------------------imports/modules end
+//setup mongoose model new creation
 
+// const getAll = async ()=> {
+//   var all =  await rModel.find({});
+//   console.log(all)
+// }
+// getAll()
+
+var routes = []
+const getAllRoutes = async ()=> {
+  let all =  await rModel.find({});
+  for(x of all){
+    console.log(x.route)
+    app.get(x.route,(request,response)=> {
+      console.log(x)
+      response.send(x)
+    })
+    
+   routes.push(x.route)
+  }
+}
+getAllRoutes()
+
+
+
+app.get("/all-routes", (request,response)=> {
+  request.send(routes)
+})
+
+
+
+app.get('/router',(request,response)=> {
+
+  response.send(routes)
+  
+})
+
+
+// user created api posting route
 app.post("/api/u-c", (req, res) => {
-  const obj = JSON.parse(JSON.stringify(req.body)); 
+  //parsing body object
+  const obj = JSON.parse(JSON.stringify(req.body));
   const route = req.body.route;
   const uFunc = req.body.funktion;
-console.log(route,uFunc)
-  // let k = new Kreator(req.body.route, req.body.function, req.body, res);
-  // k.kRoute()
-  let k = uModel.create(
+  console.log(route, uFunc);
+  let k = rModel.create(
     {
       user: "Shyaboi",
-      routes: ["/ok", route],
-      funktion: uFunc
+      route: route,
+      funktion: uFunc,
     },
-
+  
     function (err, small) {
       if (err) console.error(err);
       // saved!
       console.log("user saved");
     }
   );
-  k
+  k;
 });
 
 app.listen(port, () => {
