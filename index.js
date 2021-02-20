c60fc859209279b4059593aaa41a66b0788cbc03
Shyaboi <ian.shane.sears@gmail.com>
2021-02-20 00:23:19 -0700

    added comments

--- index.js
+++ index.js
@@ -29,16 +29,26 @@ app.use(cookieParser());
 //setup a 'cache' of all the db data
 var all;
 const getAll = async () => {
+  //getting all from mongo rModel
   all = await rModel.find({});
   //  console.log(all)
 };
 getAll();
 
 
+//all json route to get all 
 app.get('/u-c/all-JSON', (request,response)=> {
   // getAll();
   response.json(all)
 })
+
+
+
+
+
+
+
+
 //route for getting and creating the user routes from the 'cached' user created routes
 app.get("/u-c-r/:route", function (request, response) {
   //seting up the 'final' function that will run the eval and return the route
@@ -51,17 +61,24 @@ try {
     // do the function eval of the JS string...might be pretty dangerous...but lets see!
 
     var fin = await eval(result.funktion)
-  
+  //catch errors
 } catch (error) {
   response.send(error.toString())
   
 }
   }
+  //run the final function
     final()
  
 });
 
 
+
+
+
+
+
+//get all routes
 app.get("/u-c/all-routes", (request, response) => {
   var allRoutes = []
   for (route of all){
@@ -70,6 +87,11 @@ app.get("/u-c/all-routes", (request, response) => {
   response.send(allRoutes);
 });
 
+
+
+
+
+
 // user created api posting route
 app.post("/u-c/new", (req, res) => {
   //parsing body object
@@ -77,20 +99,23 @@ app.post("/u-c/new", (req, res) => {
   const route = req.body.route;
   const uFunc = req.body.funktion;
   console.log(route, uFunc);
+  //creating the model from the mongo model
   let k = rModel.create(
     {
       user: "Shyaboi",
       route: '/'+route,
       funktion: uFunc,
     },
-
+//catch errors
     function (err, small) {
       if (err) console.error(err);
       // saved!
       console.log(obj);
     }
   );
+  //run model creation
   k;
+  //run getAll to update the cached obj from DB
 getAll();
 console.log('all updated')
 });
@@ -100,10 +125,11 @@ console.log('all updated')
 
 
 
-
+// 404 catch all
 app.get('*', function(req, res) {
   res.send('<h1>Error 404</h1><h2>Not a route yet, but you can make one <a href="http://uselessapi.com/editor/">here</a>.</h2>')
 });
+//setting up server listener
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
